7945b621f89c4ee095fe464707c65d5e42c3ae63
mallow <131043006+mallowPallow@users.noreply.github.com>
2023-08-13 18:32:59 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -33,3 +33,35 @@ RegisterNetEvent('lucid-masks:server:RemoveMaskItem', function(item)
     TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[item], "remove")
 end)
 
+QBCore.Functions.CreateUseableItem("hat", function(source, item)
+    TriggerClientEvent("lucid-masks:client:HatOn", source, item)
+end)
+
+RegisterNetEvent('lucid-masks:server:HatOnByCommand', function(hat, tex)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local hat = Player.Functions.GetItemByName("hat")
+    if hat then
+        TriggerClientEvent("lucid-masks:client:MaskOn", source, hat)
+    else
+        TriggerClientEvent("QBCore:Notify", src, "You don\'t have a hat", "error")
+    end
+end)
+
+RegisterNetEvent('lucid-masks:server:GiveHatItem', function(hat, tex)
+    if hat == 0 then return end
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local info = {drawableId = hat, textureId = tex}
+
+    Player.Functions.AddItem("hat", 1, false, info)
+    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["hat"], "add")
+end)
+
+RegisterNetEvent('lucid-masks:server:RemoveHatItem', function(item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+
+    Player.Functions.RemoveItem(item.name, 1, item.slot)
+    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[item], "remove")
+end)
