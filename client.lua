cd07c0f4164eb2b9ef76d9f03d7cdce7a4e2f62f
mallow <131043006+mallowPallow@users.noreply.github.com>
2023-08-13 18:32:35 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -58,3 +58,57 @@ RegisterNetEvent('lucid-masks:client:MaskOff', function(mask, tex)
 
     end)
 end)
+
+RegisterCommand("hat", function()
+    local ped = PlayerPedId()
+    local hat = GetPedPropIndex(ped, 0)
+    local tex = GetPedPropTextureIndex(ped, 0)
+
+    if hat == -1 then
+        TriggerServerEvent("lucid-masks:server:HatOnByCommand")
+    else
+        TriggerEvent("lucid-masks:client:HatOff", hat, tex)
+    end
+end, false)
+
+RegisterNetEvent('lucid-masks:client:HatOn', function(item)
+    local ped = PlayerPedId()
+    if GetPedPropIndex(ped, 0) ~= -1 then QBCore.Functions.Notify("You are already wearing a hat", "error") return end
+
+    QBCore.Functions.Progressbar("haton", "Putting Hat On..", 600, false, true, {
+        disableMovement = false,
+        disableCarMovement = false,
+        disableMouse = false,
+        disableCombat = false,
+    }, {
+        animDict = "mp_masks@standard_car@ds@",
+        anim = "put_on_mask",
+        flags = 49,
+    }, {}, {}, function() -- Done
+        SetPedPropIndex(ped, 0, item.info.drawableId, item.info.textureId)
+        TriggerServerEvent("lucid-masks:server:RemoveHatItem", item)
+    end, function() -- Cancel
+
+    end)
+end)
+
+RegisterNetEvent('lucid-masks:client:HatOff', function(hat, tex)
+    local ped = PlayerPedId()
+    if GetPedPropIndex(ped, 0) == -1 then QBCore.Functions.Notify("You are not wearing a hat", "error") return end
+
+    QBCore.Functions.Progressbar("hatoff", "Taking hat Off..", 600, false, true, {
+        disableMovement = false,
+        disableCarMovement = false,
+        disableMouse = false,
+        disableCombat = false,
+    }, {
+        animDict = "missfbi4",
+        anim = "takeoff_mask",
+        flags = 49,
+    }, {}, {}, function() -- Done
+        ClearPedProp(ped, 0)
+        TriggerServerEvent("lucid-masks:server:GiveHatItem", hat, tex)
+    end, function() -- Cancel
+
+    end)
+end)
